ba197e39d008345c7ef7153d2678dd2dd9216624
ANA JÚLIA FARIA DA SILVA <anajulia.faria@a.ucb.br>
2026-03-30 20:39:33 -0300

    Aula 30/03

--- appweb_calculadora/index.js
+++ appweb_calculadora/index.js
@@ -1,20 +1,29 @@
 //carrega framework express
 const express = require('express');
 
+const calculadora = require('./calculadora');
+
 //cria um app usando a funcao express()
 const app     = express();
 
 //define porta de aplicação
 const PORT    = 8080;
 
+
+//URL FIXA 
 app.get('/', function (req, rest){
     rest.send('<h1> Olá, mundo!</h1> <p>Estou na Web...');
 });
 
 
-app.get('/bem-vindo', function (req, rest){
-    rest.send('<h1> Bem vindo ao desenvolvimento Web!</h1>');
+app.get('/calculadora/somar/:a/:b', (req, rest)=>{
+    let a = req.params.a;
+    let b = req.params.b;
+    let resultado = calculadora.somar(a, b);
+    rest.send(`${a} + ${b} = ${resultado} `);
 });
+
+///inicializador a aplicacao na porta definida
 
 app.listen(PORT, ()=>{
     console.log('app rodando na porta ' + PORT);
